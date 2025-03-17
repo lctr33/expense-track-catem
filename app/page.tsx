b1c825700ad70fd2ce6bf08fbd24d6cfe9ff5f6a
lctr <anibal.medina@uaem.edu.mx>
2025-03-17 12:56:02 -0600

    globals.css customization

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hola que tal
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font[family-name: var(--font-geist-sans)] bg-background text-foreground">
+      <h1 className="text-4xl [font-weight:1000] text-primary animate-fadeInUp">
+        Seguimiento de Gastos CATEM
+      </h1>
     </div>
   );
 }
