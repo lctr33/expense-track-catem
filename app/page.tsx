d4c761ffcf91e6bb01397b4821618214fdf74103
lctr <anibal.medina@uaem.edu.mx>
2025-03-17 13:16:29 -0600

    correxxión de globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 //import Image from "next/image";
-
+import { Button } from '../components/ui/button';
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font[family-name: var(--font-geist-sans)] bg-background text-foreground">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font[family-name: var(--font-geist-sans)]">
       <h1 className="text-4xl [font-weight:1000] text-primary animate-fadeInUp">
         Seguimiento de Gastos CATEM
       </h1>
+      <Button variant="outline" size={'lg'}>
+        Loggeate aquí perro
+      </Button>
     </div>
   );
 }
